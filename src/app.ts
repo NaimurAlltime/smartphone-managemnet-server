f5af5325931f5a9c3fc87bbd3b8123ae1a9c044a
NaimurAlltime <naimur2231@gmail.com>
2024-01-25 00:44:48 +0600

    initialization project setup done

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+// parser
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+// application routes
+
+//testing
+app.get("/", (req: Request, res: Response) => {
+  res.send("server in running");
+});
+
+export default app;
